5821913c8fa4f3ee941945ace9650a918f999249
NichBon <bonettn@gmail.com>
2025-03-26 09:40:24 +1100

    adds brainstormed requirements to script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// generate a word to guess at
+// make the words coding related?
+
+// confirm if a letter is in a guess
+    // check array of word
+    // return indexes of matching letter in word
+    // return some false or similar value if not present
+
+// change picture if guess is wrong (or other behaviour)
+
+// button press || key press behaviour
+    // if button or key is pressed
+    // guess letter  
+        // if letter is in a guess
+            // show letter in word
+            // turn letter background positive colour
+        //if letter is not in a guess
+            // change wrong guess counter (and change picture)
+            // turn letter background negative colour
+    // deactivate button for that letter
+        
+// success/loss script
+    // return meaning and/or syntax of the word?
+
+
+// reset state
+// record previous word or remove previous word from pool
